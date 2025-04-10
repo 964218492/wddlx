88120d41bb779bc94e879ca2d36ecc643a844148
周万超 <964218492@qq.com>
2025-04-10 10:33:27 +0800

    no message

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+const tg = window.Telegram.WebApp;
+
+tg.ready(); // 告诉 Telegram 小程序已准备就绪
+
+// 设置 UI 外观
+tg.expand(); // 展开为全屏
+tg.MainButton.setText("提交").show().onClick(() => {
+  // 点击按钮时的处理逻辑
+  tg.sendData("some_payload"); // 向 bot 发送数据
+});
+</script>
+
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: '0.0.0.0', // 默认为localhost
+    port: 8080, // 端口号
+    open: false, // 是否自动打开浏览器
+    strictPort: false
+  },
+})
